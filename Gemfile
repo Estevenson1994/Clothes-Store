d01ae4211dc83010debeb669b6e80906f6f856aa
Erin Stevenson <estevenson_94@live.com>
2019-04-12 11:10:17 +0100

    project set up

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'active_type'
+gem 'rubocop-performance', require: false
+gem 'rufo'
+gem 'sinatra'
+gem 'sinatra-flash'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+# Requiring all of the testing gems
+
+require "capybara"
+require "capybara/rspec"
+require "rspec"
+require "simplecov"
+require "simplecov-console"
+
+# Get test coverage
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+
+SimpleCov.start
+
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
