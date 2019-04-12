c94143ed106b3ea32a5ffcb5f72fec7af9e8e9ae
Erin Stevenson <estevenson_94@live.com>
2019-04-12 22:52:45 +0100

    can apply vouchers at checkout

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,16 @@
 class Checkout
-  attr_reader :cart
+  attr_reader :cart, :vouchers
 
   def initialize(cart)
     @cart = cart
+    @vouchers = []
   end
 
   def total_item_cost
     @cart.total_cost
+  end
+
+  def apply_voucher(voucher)
+    @vouchers << voucher
   end
 end
--- spec/units/checkout_spec.rb
+++ spec/units/checkout_spec.rb
@@ -4,11 +4,19 @@ RSpec.describe Checkout do
   let(:womens_shoes) { double :womens_shoes, category: "Footware", price: 42.00 }
   let(:mens_shoes) { double :mens_shoes, category: "Footware", price: 10.00 }
   let(:cart) { double :cart, basket: [womens_shoes, mens_shoes], total_cost: 52.00 }
+  let(:five_pound_voucher) { double :five_pound_voucher, amount: 5, min_spend: 0, required_item: nil }
   subject(:checkout) { described_class.new(cart) }
 
   describe "#total_item_cost" do
     it "returns total cost of all items in cart" do
       expect(checkout.total_item_cost).to eq 52.00
+    end
+  end
+
+  describe "#apply_voucher" do
+    it "adds voucher to list of vouchers" do
+      checkout.apply_voucher(five_pound_voucher)
+      expect(checkout.vouchers).to include(five_pound_voucher)
     end
   end
 end
