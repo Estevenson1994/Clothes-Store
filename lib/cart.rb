5ac32dba5850e974429ce109d03874296394d350
Erin Stevenson <estevenson_94@live.com>
2019-04-12 12:36:02 +0100

    cart is initially empty

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,7 +1,8 @@
 class Cart
-  attr_reader :total_cost
+  attr_reader :total_cost, :basket
 
   def initialize
     @total_cost = 0
+    @basket = []
   end
 end
--- spec/units/cart_spec.rb
+++ spec/units/cart_spec.rb
@@ -1,9 +1,13 @@
 require "cart"
 
 RSpec.describe Cart do
-    subject(:cart) { described_class.new }
+  subject(:cart) { described_class.new }
 
-    it "has an initial total cost of 0" do
-        expect(cart.total_cost).to eq 0
-    end
+  it "has an initial total cost of 0" do
+    expect(cart.total_cost).to eq 0
+  end
+
+  it "initially has no items in the basket" do
+    expect(cart.basket).to be_empty
+  end
 end
