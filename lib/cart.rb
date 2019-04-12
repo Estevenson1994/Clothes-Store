f5c96e5794dfdc4dc965b79491089d0908d6605d
Erin Stevenson <estevenson_94@live.com>
2019-04-12 17:21:07 +0100

    refactor out cart add item methods

--- lib/cart.rb
+++ lib/cart.rb
@@ -7,17 +7,27 @@ class Cart
   end
 
   def add_item(item)
-    if @basket.any? { |cart_item| cart_item.id == item.id }
-      cart_item = @basket.find { |cart_item| cart_item.id == item.id }
-      cart_item.increase_quantity
-    else
-      @basket << item
-    end
+    basket_contains_item(item) ? increase_item_quantity(item) : add_item_to_basket(item)
     @total_cost += item.price
   end
 
   def remove_item(item)
     @basket.delete(item)
     @total_cost -= item.price
+  end
+
+  private
+
+  def basket_contains_item(item)
+    @basket.any? { |cart_item| cart_item.id == item.id }
+  end
+
+  def increase_item_quantity(item)
+    cart_item = @basket.find { |cart_item| cart_item.id == item.id }
+    cart_item.increase_quantity
+  end
+
+  def add_item_to_basket(item)
+    @basket << item
   end
 end
