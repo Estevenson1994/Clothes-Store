4ef94c2acf8b8ac5a265adf8c2da8eed4625e3f2
Erin Stevenson <estevenson_94@live.com>
2019-04-12 22:47:00 +0100

    refactor method to find voucher by id

--- lib/voucher.rb
+++ lib/voucher.rb
@@ -1,5 +1,4 @@
 require "active_type"
-require_relative "../voucher_data"
 
 class Voucher < ActiveType::Object
   attribute :id, :string
@@ -8,14 +7,23 @@ class Voucher < ActiveType::Object
   attribute :required_item, :string
 
   def self.find(id)
-    if VOUCHERS.any? { |voucher| voucher[:id] == id }
-      voucher = VOUCHERS.select { |voucher| voucher[:id] == id }.first
-      Voucher.new(id: voucher[:id],
-                  amount: voucher[:amount],
-                  min_spend: voucher[:min_spend],
-                  required_item: voucher[:required_item])
-    else
-      raise "Invalid voucher id"
-    end
+    raise "Invalid voucher id" unless voucher_exists(id)
+    return_voucher(id)
+  end
+
+  private
+
+  require_relative "../voucher_data"
+
+  def self.voucher_exists(id)
+    VOUCHERS.any? { |voucher| voucher[:id] == id }
+  end
+
+  def self.return_voucher(id)
+    voucher = VOUCHERS.select { |voucher| voucher[:id] == id }.first
+    Voucher.new(id: voucher[:id],
+                amount: voucher[:amount],
+                min_spend: voucher[:min_spend],
+                required_item: voucher[:required_item])
   end
 end
