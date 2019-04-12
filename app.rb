082223fb45d319f1be876c6ec9d541e9b741151a
Erin Stevenson <estevenson_94@live.com>
2019-04-12 12:26:58 +0100

    can see all products available

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require "sinatra/base"
+require "./lib/product"
 
 class ClothesStore < Sinatra::Base
   get "/" do
     erb :'index'
+  end
+
+  get "/products" do
+    @products = Product.all
+    erb :'products'
   end
 end
--- spec/features/viewing_products_spec.rb
+++ spec/features/viewing_products_spec.rb
@@ -6,4 +6,14 @@ feature "Viewing Products" do
       expect(page).to have_content "Clothes Store"
     end
   end
+
+  feature "viewing products" do
+    scenario "all products are visable" do
+      visit "/"
+      click_button("View Products")
+      expect(page).to have_content("Items Available:")
+      expect(page.find(".products")).to have_selector("li", count: 13)
+      expect(page).to have_content("Women's Footwear: Almond Toe Court Shoes in Patent Black - £99.00, total stock: 5")
+    end
+  end
 end
