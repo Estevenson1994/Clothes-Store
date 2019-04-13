18cf19defcf8c641a305bb02f2bdbe2b62e4def5
Erin Stevenson <estevenson_94@live.com>
2019-04-13 09:12:45 +0100

    move all checkout feature tests to one file

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -1,4 +1,20 @@
 feature "Checkout" do
+  scenario "shows all items and total cost of all items in the basket" do
+    visit "/"
+    click_button("View Products")
+    within ".products" do
+      click_button("Add to basket", :match => :first)
+    end
+    within ".products" do
+      click_button("Add to basket", :match => :first)
+    end
+
+    click_button("Checkout")
+    expect(page).to have_content("Checkout\n")
+    expect(page).to have_content("Almond Toe Court Shoes in Patent Black - £99.00, quantity: 2")
+    expect(page).to have_content("Total item cost: £198.00")
+  end
+
   scenario "can add voucher at checkout and see the discount" do
     visit "/"
     click_button("View Products")
