8e29cf5c7883b71daf58f2486757349cdd424a00
Erin Stevenson <estevenson_94@live.com>
2019-04-14 11:21:38 +0100

    correct fifteen pound voucher min spend amount

--- data/voucher_data.rb
+++ data/voucher_data.rb
@@ -1,3 +1,3 @@
 VOUCHERS = [{ id: "Five", amount: 5, min_spend: 0 },
             { id: "Ten", amount: 10, min_spend: 50 },
-            { id: "Fifteen", amount: 15, min_spend: 74, required_item: "Footwear" }]
+            { id: "Fifteen", amount: 15, min_spend: 75, required_item: "Footwear" }]
